7a7329de72fdb36f964891fb739b20aeac6924a8
revat <revat@DESKTOP-2E3NR6Q>
2023-02-20 19:37:49 +0530

    First commit for Github3

---
+++ src/Pack1/Test00001.java
@@ -0,0 +1,17 @@
+package Pack1;
+
+public class Test00001 {
+
+	public static void main(String[] args) {
+		String str="Durva";
+		if(str=="Durva")
+		{
+			System.out.println("My name is Durva");
+		}
+		else
+		{
+			System.out.println("My name is Revati");
+		}
+	}
+
+}
